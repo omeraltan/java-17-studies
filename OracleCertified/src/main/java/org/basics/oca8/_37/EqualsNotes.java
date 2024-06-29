1a524c7ff74d0e3608b6825b1433a49e0dda0084
omeraltan <omeraltan66@gmail.com>
2024-06-29 13:15:04 +0300

    Commit All

---
+++ OracleCertified/src/main/java/org/basics/oca8/_37/EqualsNotes.java
@@ -0,0 +1,42 @@
+package org.basics.oca8._37;
+
+public class EqualsNotes {
+
+    public static void main(String[] args) {
+        Integer i1 = new Integer(10);
+        Integer i2 = new Integer(10);
+
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+
+        Long l1 = new Long(10);
+        System.out.println(i1.equals(l1));
+
+        StringBuffer sb1 = new StringBuffer("Hello");
+        StringBuffer sb2 = new StringBuffer("Hello");
+
+        System.out.println(sb1.equals(sb2));
+        // StringBuffer sınıfında equals methodu override edilmemiştir. Bu yüzden false döner.
+        System.out.println(sb1.toString().equals(sb2.toString()));
+
+
+
+
+
+    }
+
+}
+
+/**
+ *
+ * java.lang.Object sınıfında yer alan equals() methodu şu şekildedir.
+ *
+ * public boolean equals(Object obj){
+ *     return (this == obj);
+ * }
+ * buradaki equals methodu == ile aynı anlama gelmektedir.
+ * yani bu 2 referans değişken aynı objeyi mi gösteriyor?
+ *
+ * Integer sınıfında equals() methodu override edilmiştir.
+ *
+ */
---
+++ OracleCertified/src/main/java/org/basics/oca8/_37/EqualsNotes2.java
@@ -0,0 +1,34 @@
+package org.basics.oca8._37;
+
+public class EqualsNotes2 {
+
+
+
+}
+
+/**
+ *
+ * reflexive / yansımalı
+ * ---------------------
+ * x.equals(x) true olmalıdır.
+ *
+ * symetric / simetrik
+ * --------------------
+ * x.equals(y) true ise
+ * y.equals(x) true olmalıdır.
+ *
+ * transitive / geçişli
+ * --------------------
+ * x.equals(y) true ise
+ * y.equals(z) true ise
+ * x.equals(z) true dur.
+ *
+ * consistent / tutarlı
+ * --------------------
+ * x.equals(y) true ise her zaman true dönmelidir.
+ *
+ * null reference
+ * --------------------
+ * x.equals(null) false dönmelidir.
+ *
+ */
